4440b85744b9c1e64b06894e0bc07b5ea6b52432
Riiccardods <riicardoljn@icloud.com>
2023-09-03 21:54:32 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TabelaMultiplicacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número inteiro positivo para ver sua tabela de multiplicação: ");
+            int n = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($" {n} x {i} = {n * i}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
