ec6e40948efd6682ac1e015b3cecdd5ecace026c
Riiccardods <riicardoljn@icloud.com>
2023-09-15 17:50:39 -0300

    A

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace TabelaMultiplicacao
             Console.Write("Digite um número: ");
             if(int.TryParse(Console.ReadLine(), out int n))
             {
-                for (int i = 1; i <= 10; i++)
+                for (int i = 1; i <= 5; i++)
                 {
                     Console.WriteLine($" {n} x {i} = {n * i}");
                 } 
